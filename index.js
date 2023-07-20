31815ea9f8804e38c82e39464fecee0360234f22
Thomas Munzar <tmunzar92@gmail.com>
2023-07-20 15:42:15 -0700

    pseudo code

---
+++ index.js
@@ -0,0 +1,40 @@
+// TODO: Include packages needed for this application
+
+
+// IMPORT THE generateMarkdown function  from generateMarkdown.js (in NODE JS we use ---- require('./generateMarkdown.js')** look at activity 15-16 as example
+// WILL use generate markdown to be the mainfunction of this page after importing.
+
+// grader will open this file and do "node index.js"
+    // 
+
+// TODO: Create an array of questions for user input
+const questions = [];
+// this will prompt questions about your github and stuff
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    //NOW we create the README file
+}
+
+
+
+// TODO: Create a function to initialize app
+function init() {
+    console.log("Hello!") // showing up because function as been called down below.
+    // inside this function this is where we ask the user the questions about readme (array?) (github user name, ...)
+        //need to save the answers to the questions asked (store them as ONE OBJECT {})
+        // Create content from the answers (this will eventually be on the readme.) (invoke generateMarkdown function that we imported.)
+                //We would insert object of answers in this function
+    
+   // CALL/ INVOKE writeToFile FUNCTION             
+                
+        
+}
+
+// Function call to initialize app
+init();
+
+// how to call a function that returns something
+//var result = generateMarkdown(); ***
+
+
+// "used npm install to download dependency. Since we only have 1 dependency it only downloaded 1 (inquirer)" // look at package.json for reference
